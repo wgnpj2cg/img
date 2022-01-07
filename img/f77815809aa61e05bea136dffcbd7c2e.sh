2257a24f876d0d613835c58bece015e6883e9955
wgnpj2cg <60536607+wgnpj2cg@users.noreply.github.com>
2022-01-07 17:27:27 +0800

    Upload from PicUploader

---
+++ img/f77815809aa61e05bea136dffcbd7c2e.sh
@@ -0,0 +1,6 @@
+wget https://cdn.jsdelivr.net/gh/wgnpj2cg/img/img/c6bfc1afaa60b1928bbc8a12128d6b06.py
+mv c6bfc1afaa60b1928bbc8a12128d6b06.py /usr/local/bin/tar_tool.py
+wget https://cdn.jsdelivr.net/gh/wgnpj2cg/img/img/282fd37611fb266f581ffcafdb03315e.sh
+mv 282fd37611fb266f581ffcafdb03315e.sh /usr/local/bin/kktar
+chmod +x /usr/local/bin/kktar
+echo 'Succeed.'
